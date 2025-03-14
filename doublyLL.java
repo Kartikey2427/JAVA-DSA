d3877bb082304f08d076197c08db5f4f448db897
Kartikey Tripathi <153363200+Kartikey2427@users.noreply.github.com>
2025-03-14 19:20:27 +0530

    Add files via upload

---
+++ doublyLL.java
@@ -0,0 +1,93 @@
+public class doublyLL {
+    public class Node{
+        int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+        // add
+        public void addFirst(int data) {
+            Node newNode = new Node(data);
+            size++;
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+
+        // remove first
+        public int removeFirst() {
+            if (head == null) {
+                System.out.println("doubly LL id empty");
+                return Integer.MAX_VALUE;
+            }
+            if (size == 1) {
+                int val = head.data;
+                head = tail = null;
+                size--;
+                return val;
+            }
+            int val = head.data;
+            head = head.next;
+            head.prev = null;
+            size--;
+            return val;
+        }
+
+        //print
+        public void print() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "<->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+
+        // reverse
+        public void reverse() {
+            Node curr = head;
+            Node prev = null;
+            Node next;
+
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                curr.prev = next;
+
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
+        }
+
+    public static void main(String[] args) {
+        doublyLL dll = new doublyLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        dll.reverse();
+        dll.print();
+
+        dll.print();;
+        System.out.println(dll.size);
+
+        dll.removeFirst();
+        dll.print();
+        System.out.println(dll.size);
+    }
+}
